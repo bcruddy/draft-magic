abef3ca344e8837d7b8f8e914107d6b6ebbdc6c7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-29 12:56:31 +0000

    Deploy bcruddy/draft-magic to github.com/bcruddy/draft-magic.git:gh-pages

---
+++ precache-manifest.502e65d8db5bbe105cd319baaebc6bad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a8cc277660a697ca7a6e",
+    "url": "/js/chunk-vendors.a8cc2776.js"
+  },
+  {
+    "revision": "232b7992d95836a69be6",
+    "url": "/js/app.232b7992.js"
+  },
+  {
+    "revision": "822a92388f2790033be43d5b432fe380",
+    "url": "/index.html"
+  },
+  {
+    "revision": "232b7992d95836a69be6",
+    "url": "/css/app.17cdf424.css"
+  }
+];
